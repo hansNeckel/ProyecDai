96e1d4183f626da924f4dcb1f6dd9c315939fb3e
crist <crist@DESKTOP-OU2L50A>
2017-06-28 19:03:46 -0400

    Commit 1 porque se me habia olvidado subir el trabajo al git XD

---
+++ PhpProject1/Index.php
@@ -0,0 +1,29 @@
+<html>  
+    <head>
+    <title>Taxis De Lujo</title>
+  <link href="css/style.css" rel="stylesheet" type="text/css"/>
+  </head>
+  <body>
+ 
+  
+
+
+    <div class="menu">
+        <center>
+            
+        <a href="index.php" >Inicio</a>
+        
+        <a href="servicios.php" >Servicios</a>
+     
+        <a href="registro.php" >Registro</a>
+        
+        <a href="Contacto.php" >Contacto</a>
+        
+        <a href="ayuda.php" >Ayuda</a>
+          
+        <a href="Login.php" >Iniciar Sesión</a>
+        </center>
+    </div>
+ 
+  </body>
+</html>
---
+++ PhpProject1/Login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head><title>Login</title></head>
+    <body>
+	<div>
+          
+                <link href="../css/style.css" rel="stylesheet" type="text/css"/>
+		<h1>Login</h1>
+                <form method="POST" action="VerificarLogin.php">
+		<table>
+		<tr>
+		<td><input type="text" required name="nombre" placeholder="Correo"/></td>
+		</tr>
+		<tr>
+		<td><input type="password" required name="clave" placeholder="Contraseña" /></td>
+		</tr>
+		</table>
+		<button type="submit">Ingresar</button>
+
+        </div>
+    </body>
+</html>
